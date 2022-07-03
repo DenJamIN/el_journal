6f25dbbef6827fd0ab51d545799ae6e548d813de
lavrentiev-d <denlavre@mail.ru>
2022-07-03 13:46:44 +0300

    переименование main.dart->JournalsList.dart

---
+++ JournalsList.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'ToolsBar.dart';
+
+void main() {
+  runApp(const JournalsList());
+}
+
+class JournalsList extends StatelessWidget {
+  const JournalsList({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Электронный журнал',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: const ToolsBar(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
