eeb57ea5fa7d44cc471af7857070019511ca76c6
lavrentiev-d <denlavre@mail.ru>
2022-07-05 17:01:07 +0300

    ToolsBar.dart: содержимое Navigator перенесено в отдельный файл JournalConstructorPage.dart

---
+++ pages/JournalConstructorPage.dart
@@ -0,0 +1,42 @@
+
+import 'package:flutter/material.dart';
+
+
+class JournalConstructorPage extends StatelessWidget{
+  const JournalConstructorPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      initialIndex: 1,
+      length: 2,
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text('Конструктор журнала'),
+          bottom: const TabBar(
+            tabs: <Widget>[
+              Tab(
+                text: 'Форма заполнения',
+                icon: Icon(Icons.edit),
+              ),
+              Tab(
+                text: 'Список групп',
+                icon: Icon(Icons.list),
+              ),
+            ],
+          ),
+        ),
+        body: const TabBarView(
+          children: <Widget>[
+            Center(
+              child: Text("Поля для заполнения журнала"),
+            ),
+            Center(
+              child: Text("Список действующих групп, пока подумать зачем"),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
