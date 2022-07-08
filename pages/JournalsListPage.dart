eabcb456b2d39c22ef53b18a53c47a50b5ac6448
lavrentiev-d <denlavre@mail.ru>
2022-07-09 00:10:57 +0300

    pages: JournalsListPage.dart форматирование методов построения и генерации списков под стандартный стиль block-body

--- pages/JournalsListPage.dart
+++ pages/JournalsListPage.dart
@@ -8,31 +8,35 @@ import '../builders/PopupMenuBuilder.dart';
 //-Шапка. На ней расположены: название приложения(?), панель инструментов
 class JournalsListPage extends StatefulWidget {
   const JournalsListPage({Key? key}) : super(key: key);
+
   @override
-  State<JournalsListPage> createState() => _JournalsListPage();
+  State<JournalsListPage> createState() {return _JournalsListPage();}
 }
-//TODO переписать все вызовы и построения
+
 class _JournalsListPage extends State<JournalsListPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar:AppBar(
-        title: const Text('Электронный журнал'),
-        actions: <Widget>[
-          IconButton(
-            padding: const EdgeInsets.only(left: 0.0,top: 0.0, right: 5.0, bottom: 0.0),
-            icon: const Icon(Icons.add_box),
-            onPressed: () {
-              Navigator.push(context, MaterialPageRoute<void>(
-                builder: (context) => const JournalConstructorPage()
-              ));
-            },
-          ),
-          PopupMenuButton<String>(
-            itemBuilder: PopupMenuBuilder.buildPopupMenuItems(),
-          ),
-        ],
-      ),
+        appBar: AppBar(
+          title: const Text('Электронный журнал'),
+          actions: <Widget>[
+            IconButton(
+              padding: const EdgeInsets.only(
+                  left: 0.0, top: 0.0, right: 5.0, bottom: 0.0),
+              icon: const Icon(Icons.add_box),
+              tooltip: "Добавить журнал",
+              onPressed: () {
+                Navigator.push(context, MaterialPageRoute<void>(
+                    builder: (context) => const JournalConstructorPage()
+                ));
+              },
+            ),
+            PopupMenuButton(
+              itemBuilder: PopupMenuBuilder.buildPopupMenuItems(),
+              tooltip: "Настройки",
+            ),
+          ],
+        ),
         body: Center(
             child: Column(
                 mainAxisAlignment: MainAxisAlignment.center,
@@ -47,13 +51,14 @@ class _JournalsListPage extends State<JournalsListPage> {
     );
   }
 
+
+  //TODO придумать куда и как перекинуть кусок построения DataTable(проблема в setState())
   List<Journal> journals = JournalList.journals;
   int? sortColumnIndex;
   bool isAscending = false;
 
   Widget buildDataTable(){
     final columns = ['Дисциплина','Группа'];
-
     return DataTable(
       sortAscending: isAscending,
       sortColumnIndex: sortColumnIndex,
@@ -62,23 +67,26 @@ class _JournalsListPage extends State<JournalsListPage> {
     );
   }
 
-  List<DataColumn> getColumns(List<String> columns) =>
-      columns.map((String column) =>
-          DataColumn(
-              label: Text(column),
-              onSort: onSort
-          )
-      ).toList();
+  List<DataColumn> getColumns(List<String> columns) {
+    return columns
+        .map((String c) => DataColumn(label: Text(c), onSort: onSort))
+        .toList();
+  }
 
-  List<DataRow> getRows(List<Journal> journals) => journals
-      .map((Journal journal) {
-    final cells = [journal.discipline.toString(), journal.group.toString()];
-    return DataRow(cells: getCells(cells));
-  }).toList();
+  List<DataRow> getRows(List<Journal> journals) {
+    return journals
+      .map((Journal j) {
+        final cells = [j.discipline.toString(), j.group.toString()];
+        return DataRow(cells: getCells(cells));
+      })
+      .toList();
+  }
 
-  List<DataCell> getCells(List<dynamic> cells) => cells
-      .map((cell) => DataCell(Text('$cell')
-  )).toList();
+  List<DataCell> getCells(List<dynamic> cells) {
+    return cells
+        .map((cl) => DataCell(Text('$cl')))
+        .toList();
+  }
 
   void onSort(int columnIndex, bool ascending){
     if(columnIndex == 0){
@@ -87,8 +95,8 @@ class _JournalsListPage extends State<JournalsListPage> {
       journals.sort((a,b)=>compareString(ascending, a.group.toString(), b.group.toString()));
     }
     setState(() {
-      this.sortColumnIndex = columnIndex;
-      this.isAscending = ascending;
+      sortColumnIndex = columnIndex;
+      isAscending = ascending;
     });
   }
 
