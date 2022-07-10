1f2ae54b6ef7e910985a2f43c14e8011e9090a87
lavrentiev-d <denlavre@mail.ru>
2022-07-10 15:20:52 +0300

    pages: MainPage.dart реализован BottomNavigationTab

--- pages/MainPage.dart
+++ pages/MainPage.dart
@@ -1,21 +1,69 @@
+import 'package:el_journal/pages/PersonProfilePage.dart';
 import 'package:flutter/material.dart';
 
 import 'JournalsListPage.dart';
 
 void main() {
-  runApp(const JournalsList());
+  runApp(const MainPage());
 }
 
-class JournalsList extends StatelessWidget {
-  const JournalsList({Key? key}) : super(key: key);
+class MainPage extends StatefulWidget{
+  const MainPage({Key? key}) : super(key: key);
+
+  @override
+  State<MainPage> createState() {return _MainPage();}
+}
+
+class _MainPage extends State<MainPage> {
+  int tabIndex = 1;
+  late List<Widget> listScreens;
+
+  @override
+  void initState() {
+    super.initState();
+    listScreens = [
+      const PersonProfilePage(),
+      const JournalsListPage()
+    ];
+  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Электронный журнал',
       theme: ThemeData(primarySwatch: Colors.blue),
-      home: const JournalsListPage(),
+      home: buildPageContent(),
       debugShowCheckedModeBanner: false,
     );
+  }
+
+  Widget buildPageContent(){
+    return Scaffold(
+      body: listScreens[tabIndex],
+      bottomNavigationBar: buildBottomNavigationBar(),
+      backgroundColor: Theme.of(context).primaryColor,
+    );
+  }
+
+  Widget buildBottomNavigationBar(){
+    return BottomNavigationBar(
+        selectedItemColor: Colors.white,
+        unselectedItemColor: Colors.grey[400],
+        backgroundColor: Theme.of(context).primaryColor,
+        currentIndex: tabIndex,
+        onTap: (int index) {
+          setState(() {
+            tabIndex = index;
+          });
+        },
+        items: getTabs()
+    );
+  }
+
+  List<BottomNavigationBarItem> getTabs(){
+    return [
+      const BottomNavigationBarItem(label: 'Профиль', icon: Icon(Icons.person)),
+      const BottomNavigationBarItem(label: 'Журналы', icon: Icon(Icons.menu_book))
+    ];
   }
 }
