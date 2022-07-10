db25e19b47e83afdc66ef33bbf93cd480c6af8c2
lavrentiev-d <denlavre@mail.ru>
2022-07-10 15:20:18 +0300

    pages: JournalsListPage.dart форматирование

--- pages/JournalsListPage.dart
+++ pages/JournalsListPage.dart
@@ -7,7 +7,7 @@ import '../builders/PopupMenuBuilder.dart';
 import 'JournalPage.dart';
 
 //-Шапка. На ней расположены: название приложения(?), панель инструментов
-class JournalsListPage extends StatefulWidget {
+class JournalsListPage extends StatefulWidget{
   const JournalsListPage({Key? key}) : super(key: key);
 
   @override
@@ -18,37 +18,37 @@ class _JournalsListPage extends State<JournalsListPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: const Text('Электронный журнал'),
-          actions: <Widget>[
-            IconButton(
-              padding: const EdgeInsets.only(
-                  left: 0.0, top: 0.0, right: 5.0, bottom: 0.0),
-              icon: const Icon(Icons.add_box),
-              tooltip: "Добавить журнал",
-              onPressed: () {
-                Navigator.push(context, MaterialPageRoute<void>(
-                    builder: (context) => const JournalConstructorPage()
-                ));
+      appBar: AppBar(
+        title: const Text('Электронный журнал'),
+        actions: <Widget>[
+          IconButton(
+            padding: const EdgeInsets.only(
+                left: 0.0, top: 0.0, right: 5.0, bottom: 0.0),
+            icon: const Icon(Icons.add_box),
+            tooltip: "Добавить журнал",
+            onPressed: () {
+              Navigator.push(context, MaterialPageRoute<void>(
+                  builder: (context) => const JournalConstructorPage()
+              ));
               },
-            ),
-            PopupMenuButton(
-              itemBuilder: PopupMenuBuilder.buildPopupMenuItems(),
-              tooltip: "Настройки",
-            ),
-          ],
-        ),
-        body: Center(
-            child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  Container(
-                    margin: const EdgeInsets.all(10),
-                    child: buildDataTable(),
-                  ),
-                ]
-            )
-        )
+          ),
+          PopupMenuButton(
+            itemBuilder: PopupMenuBuilder.buildPopupMenuItems(),
+            tooltip: "Настройки",
+          ),
+        ],
+      ),
+      body: Center(
+          child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Container(
+                  margin: const EdgeInsets.all(10),
+                  child: buildDataTable(),
+                ),
+              ]
+          )
+      ),
     );
   }
 
