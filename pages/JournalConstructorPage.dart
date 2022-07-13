f24849210a535b6953b117c0bbc2b217b2ccfba4
lavrentiev-d <denlavre@mail.ru>
2022-07-13 18:25:01 +0300

    pages: JournalConstructorPage.dart добавлен TextButton; +todo на обработку TextFields

--- pages/JournalConstructorPage.dart
+++ pages/JournalConstructorPage.dart
@@ -1,4 +1,6 @@
 
+// ignore_for_file: non_constant_identifier_names
+
 import 'package:flutter/material.dart';
 
 //TODO заполнить страницу
@@ -6,6 +8,7 @@ import 'package:flutter/material.dart';
 //-Поля имени, группы или возможность сделать пользовательский журнал
 class JournalConstructorPage extends StatelessWidget{
   const JournalConstructorPage({Key? key}) : super(key: key);
+  final EdgeInsets DEFAULT_EDGES_INSETS = const EdgeInsets.only(left: 30, top: 10, right: 30, bottom: 10);
 
   @override
   Widget build(BuildContext context) {
@@ -15,32 +18,61 @@ class JournalConstructorPage extends StatelessWidget{
         ),
         body: Column(
             mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[//TODO сделать генерацию
+            children: <Widget>[//TODO заполнить onSubmitted
               Container(
-                margin: const EdgeInsets.only(left: 30, top: 10, right: 30, bottom: 10),
+                margin: DEFAULT_EDGES_INSETS,
                 child: const TextField(
                   decoration: InputDecoration(
                     border: OutlineInputBorder(),
                     labelText: 'Название журнала',
                   ),
+                  //!onSubmitted пользователь закончил ввод текста
                 ),
               ),
               Container(
-                margin: const EdgeInsets.only(left: 30, top: 10, right: 30, bottom: 10),
+                margin: DEFAULT_EDGES_INSETS,
                 child: const TextField(
                   decoration: InputDecoration(
                     border: OutlineInputBorder(),
                     labelText: 'Дисциплина',
                   ),
+                  //!onSubmitted пользователь закончил ввод текста
                 ),
               ),
               Container(
-                margin: const EdgeInsets.only(left: 30, top: 10, right: 30, bottom: 10),
+                margin: DEFAULT_EDGES_INSETS,
                 child: const TextField(
                   decoration: InputDecoration(
                     border: OutlineInputBorder(),
                     labelText: 'Академическая группа',
                   ),
+                  //!onSubmitted пользователь закончил ввод текста
+                ),
+              ),
+              const SizedBox(height: 30),
+              ClipRRect(
+                borderRadius: BorderRadius.circular(4),
+                child: Stack(
+                  children: <Widget>[
+                    Positioned.fill(
+                      child: Container(
+                        decoration: const BoxDecoration(
+                          color: Color(0xFF42A5F5),
+                        ),
+                      ),
+                    ),
+                    TextButton(
+                      style: TextButton.styleFrom(
+                        padding: const EdgeInsets.all(16.0),
+                        primary: Colors.white,
+                        textStyle: const TextStyle(fontSize: 20),
+                      ),
+                      onPressed: () {
+
+                      },
+                      child: const Text('Добавить журнал'),
+                    ),
+                  ],
                 ),
               ),
             ]
