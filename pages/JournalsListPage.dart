04e516de987b595c165ce14bd9e68eac87e3fa79
lavrentiev-d <denlavre@mail.ru>
2022-07-15 18:35:00 +0300

    widget, pages: из JournalsListPage.dart перенесено создание виджета DataTable в JournalsListDataTableWidget.dart

--- pages/JournalsListPage.dart
+++ pages/JournalsListPage.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 
 import '../builders/BuilderLists.dart';
 import '../entities/Journal.dart';
+import '../widgets/JournalsListDataTableWidget.dart';
 import '../widgets/PopupMenuWidget.dart';
 import 'JournalPage.dart';
 
@@ -42,73 +43,12 @@ class _JournalsListPage extends State<JournalsListPage> {
                 children: <Widget>[
                   Container(
                     margin: const EdgeInsets.all(10),
-                    child: buildDataTable(),
+                    child: JournalsListDataTableWidget(),
                   ),
                 ]
             )
         ),
       ),
     );
-  }
-
-
-  //TODO придумать куда и как перекинуть кусок построения DataTable(проблема в setState())
-  List<Journal> journals = JournalList.journals;
-  int? sortColumnIndex;
-  bool isAscending = false;
-
-  Widget buildDataTable(){
-    final columns = ['Дисциплина','Группа'];
-    return DataTable(
-      sortAscending: isAscending,
-      sortColumnIndex: sortColumnIndex,
-      columns: getColumns(columns),
-      rows: getRows(journals),
-    );
-  }
-
-  List<DataColumn> getColumns(List<String> columns) {
-    return columns
-        .map((String c) => DataColumn(label: Text(c), onSort: onSort))
-        .toList();
-  }
-
-  List<DataRow> getRows(List<Journal> journals) {
-    return journals
-      .map((Journal j) {
-        final cells = [j.discipline.toString(), j.group.toString()];
-        return DataRow(cells: getCells(cells));
-      })
-      .toList();
-  }
-
-  //TODO придумать обработку элемента из DataTable
-  List<DataCell> getCells(List<String> cells) {
-    return cells
-        .map((cl) => DataCell(
-                        onDoubleTap: (){
-                          Navigator.push(context, MaterialPageRoute<void>(
-                              builder: (context) => JournalPage(cells)
-                          ));
-                        },
-                        Text(cl)
-                     ))
-        .toList();
-  }
-
-  void onSort(int columnIndex, bool ascending){
-    if(columnIndex == 0){
-      journals.sort((a,b)=>compareString(ascending, a.discipline.toString(), b.discipline.toString()));
-    } else if (columnIndex == 1){
-      journals.sort((a,b)=>compareString(ascending, a.group.toString(), b.group.toString()));
-    }
-    setState(() {
-      sortColumnIndex = columnIndex;
-      isAscending = ascending;
-    });
-  }
-
-  int compareString(bool ascending, String value1, String value2){
-    return ascending ? value1.compareTo(value2) : value2.compareTo(value1);
   }
 }
