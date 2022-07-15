c024e2cdcdcd1fbeb02b72611d48758cc09f8cca
lavrentiev-d <denlavre@mail.ru>
2022-07-15 18:25:34 +0300

    widgets: переработан PopupMenuBuilder.dart -> PopupMenuWidget.dart

---
+++ widgets/PopupMenuWidget.dart
@@ -0,0 +1,19 @@
+
+import 'package:flutter/material.dart';
+
+import '../builders/BuilderLists.dart';
+
+class PopupMenuWidget extends StatelessWidget{
+  List<String> tools = SettingsTools.toolsTitles;
+
+  @override
+  Widget build(BuildContext context) {
+    return PopupMenuButton(
+      itemBuilder: (BuildContext context) {
+        return tools
+            .map((String tool) => PopupMenuItem<String>(value: tool, child: Text(tool)))
+            .toList();
+      },
+    );
+  }
+}
